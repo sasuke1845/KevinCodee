c86256392837ed0a64193d8e32fce3c07933aec3
sasuke1845 <agianakjb@gmail.com>
2024-08-30 13:28:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ display_welcome() {
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${BLUE}[+]                AUTO INSTALLER THEMA             [+]${NC}"
-  echo -e "${BLUE}[+]                  © KEVIN CODEE                [+]${NC}"
+  echo -e "${BLUE}[+]                  © KEVIN CODEE                  [+]${NC}"
   echo -e "${BLUE}[+]                                                 [+]${NC}"
   echo -e "${RED} [+] =============================================== [+]${NC}"
   echo -e ""
